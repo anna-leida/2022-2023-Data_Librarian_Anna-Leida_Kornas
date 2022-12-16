56ab930b45a58eb24ac02dcb32b316b4ec86bdfa
Anna-Leida Kornas <anna-leida.kornas@web.de>
2022-12-16 17:08:22 +0100

    Add final shell task

--- Modul_1/befehle.sh
+++ Modul_1/befehle.sh
@@ -1,11 +1,12 @@
-#Shell Skript Aufgabe 3 
+#Shell Skript Modul 1 Aufgabe 3 
 
-cut -f 5,12 2022-11-28-Article_list_dirty.tsv |
-sort -n |
-uniq |
+sed 's/IMPORTANT\t\t//g' 2022-11-28-Article_list_dirty.tsv | 
+cut -f 5,12 |
 grep -v  '^#' |
 grep -v '^$' |
 sed 's/[iI][sS][sS][nN]://' |
 sed 's/ISSN//' |
 sed 's/ *Date//' |
-sed 's/ *//'  > 2021-11-28-Dates_and_ISSNs.tsv 
+sed 's/ *//' |
+sort -n |
+uniq  > 2021-11-28-Dates_and_ISSNs.tsv 
